61a44f896ffaed0bc1819bb6dbc77eca7643816a
kokosko <kostya.kokosko@gmail.com>
2015-06-10 14:07:29 +0300

    fix some bugs

--- browserbadge.rb
+++ browserbadge.rb
@@ -16,17 +16,20 @@ get '/:browser' do
 end
 
 get '/:browser/:version' do
-  file = "public/icons/users/#{params[:browser]}-#{params[:version]}.svg"
-  text = "<rect x='13' y='22' width='18' height='8' rx='1' fill='#555'/>
-    <text font-size='8' x='14' y='29' fill='#fff'>#{params[:version]}+</text>"
-
-  unless File.exist?(file)
-    code = File.open("public/icons/default/#{params[:browser]}.svg").read
-    code.insert(code.index('</svg>'), text)
-    File.open(file, 'w') { |f| f.write(code) }
+  browsers = %w(chrome opera firefox ie safari)
+  if browsers.include?(params[:browser])
+    file = "public/icons/users/#{params[:browser]}-#{params[:version]}.svg"
+    text = "<rect x='13' y='22' width='18' height='8' rx='1' fill='#555'/>
+      <text font-size='8' x='14' y='29' fill='#fff'>#{params[:version]}+</text>"
+    unless File.exist?(file)
+      code = File.open("public/icons/default/#{params[:browser]}.svg").read
+      code.insert(code.index('</svg>'), text)
+      File.open(file, 'w') { |f| f.write(code) }
+    end
+    send_file file
+  else
+    erb :error
   end
-
-  send_file file
 end
 
 not_found do
