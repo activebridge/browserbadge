8aac03e31d839da4cd6e59e7a5f3d44c72539b67
kokosko <kostya.kokosko@gmail.com>
2015-06-11 18:55:01 +0300

    icon reg exp layout not_found

--- browserbadge.rb
+++ browserbadge.rb
@@ -6,32 +6,24 @@ get '/' do
   erb :index
 end
 
-get '/:browser' do
-  file = "public/icons/default/#{params[:browser]}.svg"
-  if File.exist?(file)
-    send_file file
+get /\A\/(chrome|opera|firefox|ie|safari)(?(3)\/|\/?)([0-9]{1,3})?(\/?)\z/ do |browser, version, _third|
+  if version.nil?
+    file = "public/icons/default/#{browser}.svg"
   else
-    erb :error
+    file = "public/icons/users/#{browser}-#{version}.svg"
+    text = "<rect x='13' y='22' width='18' height='8' rx='1' fill='#555'/>
+      <text font-size='8' x='14' y='29' fill='#fff'>
+      #{version}+</text>"
   end
-end
-
-get '/:browser/:version' do
-  browsers = %w(chrome opera firefox ie safari)
-  if browsers.include?(params[:browser])
-    file = "public/icons/users/#{params[:browser]}-#{params[:version]}.svg"
-    unless File.exist?(file)
-      text = "<rect x='13' y='22' width='18' height='8' rx='1' fill='#555'/>
-        <text font-size='8' x='14' y='29' fill='#fff'>#{params[:version]}+</text>"
-      code = File.open("public/icons/default/#{params[:browser]}.svg").read
-      code.insert(code.index('</svg>'), text)
-      File.open(file, 'w') { |f| f.write(code) }
-    end
-    send_file file
-  else
-    erb :error
+  unless File.exist?(file)
+    file = "public/icons/users/#{browser}-#{version}.svg"
+    code = File.open("public/icons/default/#{browser}.svg").read
+    code.insert(code.index('</svg>'), text)
+    File.open(file, 'w') { |f| f.write(code) }
   end
+  send_file file
 end
 
 not_found do
-  erb :error
+  send_file 'public/icons/default/not_found.svg'
 end
